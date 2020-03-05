b1f8e221dccf6945c6d9fec827051cb5b182bda2
Mike Reinders <mike.reinders.gg@gmail.com>
2020-03-05 19:50:32 +0100

    Removed odd merge command code

--- src/reinders/mike/TCsARSavegameTool/CommandTool/MergeCommand.java
+++ src/reinders/mike/TCsARSavegameTool/CommandTool/MergeCommand.java
@@ -19,31 +19,7 @@ public class MergeCommand extends Command {
     }
 
     @Override
-    public boolean execute() {
-        /*int sourceFilesCount = (args.length - 1);
-            if (sourceFilesCount <= 1) {
-                Path curFilePath = Paths.get(MergeTool.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toAbsolutePath();
-                System.out.println("java -jar " + curFilePath.getFileName() + " [target file] [source file] [source file] [... additional source files]");
-                return;
-            }
-
-            Path targetPath = Paths.get(args[0]).toAbsolutePath();
-            System.out.println("Target File: " + targetPath.getFileName());
-
-            Path sourcePath;
-            ArkSavFile sourceFile;
-            for (int i = 1; i <= sourceFilesCount; i++) {
-                sourcePath = Paths.get(args[i]).toAbsolutePath();
-                System.out.println("Processing source file #" + i + " " + sourcePath.getFileName());
-
-                PropertyInt playerVersion = (PropertyInt)sourceFile.getProperty("PlayerVersion");
-                System.out.println("PlayerVersion: " + playerVersion.getValue());
-
-                ArkArray<?> playerData = ((PropertyArray)sourceFile.getProperty("PlayerData")).getValue();
-
-                System.out.println(playerData.size() + " Players have been processed.");
-            }
-            System.out.println("Finished merging " + sourceFilesCount + " files.");*/
+    public boolean execute() throws Throwable {
 
         return true;
     }
