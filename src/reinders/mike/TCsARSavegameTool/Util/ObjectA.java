bee3f47eaa2fc6b1a2f7fd0b4ce1b153b80ed601
Mike Reinders <mike.reinders.gg@gmail.com>
2020-03-06 02:02:34 +0100

    Added setPrivateField

--- src/reinders/mike/TCsARSavegameTool/Util/ObjectA.java
+++ src/reinders/mike/TCsARSavegameTool/Util/ObjectA.java
@@ -1,7 +1,5 @@
 package reinders.mike.TCsARSavegameTool.Util;
 
-import qowyn.ark.ArkSavFile;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
@@ -13,12 +11,36 @@ public final class ObjectA {
 
     public static Object getPrivateField(Object obj, String property) {
         try {
-            Field field = obj.getClass().getDeclaredField("className");
+            Field field = ObjectA.openPrivateField(obj, property);
+
+            if (field != null) {
+                return field.get(obj);
+            }
+        } catch (IllegalAccessException ignored) {}
+        return null;
+    }
+
+    public static Object setPrivateField(Object obj, String property, Object value) {
+        try {
+            Field field = ObjectA.openPrivateField(obj, property);
+
+            if (field != null) {
+                field.set(obj, value);
+                return true;
+            }
+        } catch (IllegalAccessException ignored) {}
+
+        return false;
+    }
+
+    public static Field openPrivateField(Object obj, String property) {
+        try {
+            Field field = obj.getClass().getDeclaredField(property);
             if (Modifier.isPrivate(field.getModifiers())) {
                 field.setAccessible(true);
             }
-            return field.get(obj);
-        } catch (NoSuchFieldException | IllegalAccessException ignored) {
+            return field;
+        } catch (NoSuchFieldException ignored) {
             return null;
         }
     }
