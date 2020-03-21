a5ad73491d884980acc8dcd91b77b170f4c5418c
Mike Reinders <mike.reinders.gg@gmail.com>
2020-03-21 09:55:29 +0100

    version 1.7.1 (+0.1.1)

--- src/reinders/mike/TCsARSavegameTool/SavegameTool.java
+++ src/reinders/mike/TCsARSavegameTool/SavegameTool.java
@@ -5,7 +5,7 @@ import reinders.mike.TCsARSavegameTool.Util.ThrowableC;
 
 public final class SavegameTool {
 
-    public static final String MERGE_TOOL_VERSION = "1.6";
+    public static final String MERGE_TOOL_VERSION = "1.7.1";
 
     private static reinders.mike.TCsARSavegameTool.Command.CommandManager commandManager = new CommandManager();
 
