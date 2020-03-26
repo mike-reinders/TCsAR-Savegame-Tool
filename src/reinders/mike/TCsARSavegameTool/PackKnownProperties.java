0d8276b1c18db5aee1cb19e244a0edc022da4efc
Mike Reinders <mike.reinders.gg@gmail.com>
2020-03-26 23:03:48 +0100

    Added Pack Known Properties

---
+++ src/reinders/mike/TCsARSavegameTool/PackKnownProperties.java
@@ -0,0 +1,60 @@
+package reinders.mike.TCsARSavegameTool;
+
+import qowyn.ark.types.ArkName;
+
+public class PackKnownProperties {
+
+    /** SAVEGAME **/
+    public static final String CLASS_NAME = "className";
+    public static final String MOD_VERSION = "ModVersion";
+    public static final String PACK_DATA = "PackData";
+
+    /** PACK **/
+    public static final String PID = "PID_17_0DBB0B3F4B979711DC4FE0B013DE2465";
+    public static final String POSITION = "Position_14_1829B70540B48DD03A8540901A812A09";
+    public static final String COST = "Cost_13_E1F9E12F44B32B03DDCC2A95EECF3B48";
+    public static final String DESCRIPTION = "Desc_43_33004ECB4E8BF9365088BDAAF0949C9A";
+    public static final String CATEGORY = "Category_42_F756B72C485769725316DE93A1702785";
+    public static final String COLOUR = "Colour_55_76F586A2459955AEBC3BB3BED20EE563";
+
+    public static final String ITEMS = "Items_4_281D3F6A4CE0A0650265BCAEE4A0C320";
+
+    /** ITEM **/
+    public static final String ITEM_NAME = "Name_2_356CB04B41D9CDC8ED731D88EDE832BB";
+    public static final String ITEM_TYPE = "Type_38_5A581D9542F5287A4A50AB99E6A1741E";
+    public static final String ITEM_CLASS = "ItemClass_47_2C2B87174DA8E1103B0D49ADBB76793D";
+    public static final String ITEM_IS_BLUEPRINT = "IsBlueprint_10_888691DB4776F085504261979EAC8E76";
+    public static final String ITEM_QUALITY_NAME = "QualityName_31_0EBB39634D320255FDA2C98DD3A8F903";
+    public static final String ITEM_QUALITY_COLOUR = "QualityColour_34_16FBB90D479214334CF4F5AE3C3CA21D";
+    public static final String ITEM_QUALITY = "Quality_13_7581441341CC65F1C9740C8CB63308D3";
+    public static final String ITEM_QUANTITY = "Quantity_20_73A94EC54381B41CEF6BA5BC968A2DF0";
+    public static final String ITEM_ITEM_RATING = "ItemRating_37_39F0119148FC8FDC70BABEAAC4D5F79D";
+    public static final String ITEM_STAT_DATA = "StatData_25_148E582249E43A8BD5525AB17DDC0660";
+
+    /** ITEM - STAT **/
+    public static final String ITEM_STAT_NAME = "StatName_8_D584E8094C8FD39ED651BFA6753B947A";
+    public static final String ITEM_STAT_DISPLAYED = "DisplayedStat_2_10ACDA1C4FC410A1190B609857D7E76C";
+    public static final String ITEM_STAT_CALCULATED = "CalculatedValue_5_E81A6DA544BBE981DEE483B6921CC2FE";
+    public static final String ITEM_STAT_IS_USED = "IsStatUsed_10_B138CD9942C69CAC9549D7A014B37247";
+
+    /** ITEM **/
+    public static final String ITEM_IS_MULTIPLE_CHOICE = "IsMultipleChoice_46_C94D5CD7479D7CA934E920A8A0FEACD0";
+
+    /** PACK **/
+    public static final String DINOS = "Dinos_30_1E2438794897C7B1CD275E933B08B441";
+    public static final String REQUIREMENTS = "Requirements_52_25F3CEE245340E434627EF913CFAEE2C";
+    public static final ArkName PACK_REQUIREMENTS_STRUCT_TYPE = ArkName.constantPlain("TCsAR_PlayerPackRequirements_Struct");
+
+    /** PACK - REQUIREMENTS **/
+    public static final String REQUIREMENTS_ADMIN_ONLY = "AdminOnly_22_D220F2064E2B70BD6CCD4699068442EF";
+    public static final String REQUIREMENTS_IS_PREREQUISITE = "IsPrerequisite_29_0DB84E7446713AF39DC22DAD61FEDCCF";
+    public static final String REQUIREMENTS_PREREQUISITE_PID = "prerequisitePID_27_BD19FD70425434E477DB4A89162ADAE3";
+    public static final String REQUIREMENTS_LEVEL_RESTRICTION = "LevelRestriction_17_F638F70241CE0FBD018FABB1959C1F2E";
+    public static final String REQUIREMENTS_PURCHASE_LIMIT = "PurchaseLimit_18_468232D544DFD9DACEEF95B79C7271F5";
+    public static final String REQUIREMENTS_PURCHASE_COOLDOWN = "PurchaseCooldown_19_A8ABD1D94E98DE97FC47CDBA7AB3569F";
+    public static final String REQUIREMENTS_CUSTOM_TAGS = "CustomTags_20_24AF81344E377E1211F54DBBE9E88725";
+
+    /** PACK **/
+    public static final String PACK_VERSION = "PackVersion_58_FD5A2B18417006A28DD835BC52E780A6";
+
+}
