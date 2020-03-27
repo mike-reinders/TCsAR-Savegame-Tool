2e1d7cec1894e70c691f5d7e4ccbcf8b722ef454
Mike Reinders <mike.reinders.gg@gmail.com>
2020-03-27 19:24:38 +0100

    Added Color

--- src/reinders/mike/TCsARSavegameTool/Color.java
+++ src/reinders/mike/TCsARSavegameTool/Color.java
@@ -2,6 +2,22 @@ package reinders.mike.TCsARSavegameTool;
 
 public class Color {
 
+    public float R;
+    public float G;
+    public float B;
+    public float A;
 
+    public Color(float R, float G, float B) {
+        this.R = R;
+        this.G = G;
+        this.B = B;
+    }
+
+    public Color(float R, float G, float B, float A) {
+        this.R = R;
+        this.G = G;
+        this.B = B;
+        this.A = A;
+    }
 
 }
